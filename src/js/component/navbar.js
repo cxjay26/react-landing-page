1636d872242cd63888c4a31021f7c7d9361bcd04
cxjay26 <christianv042393@gmail.com>
2019-08-07 14:50:51 +0000

    added components and views

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -1,19 +1,61 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import {
+	Collapse,
+	Navbar,
+	NavbarToggler,
+	NavbarBrand,
+	Nav,
+	NavItem,
+	NavLink,
+	UncontrolledDropdown,
+	DropdownToggle,
+	DropdownMenu,
+	DropdownItem
+} from "reactstrap";
 
-export class Navbar extends React.Component {
+export class Navi extends React.Component {
+	constructor(props) {
+		super(props);
+
+		this.toggle = this.toggle.bind(this);
+		this.state = {
+			isOpen: false
+		};
+	}
+	toggle() {
+		this.setState({
+			isOpen: !this.state.isOpen
+		});
+	}
 	render() {
 		return (
-			<nav className="navbar navbar-light bg-light mb-3">
-				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-				</Link>
-				<div className="ml-auto">
-					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
-					</Link>
-				</div>
-			</nav>
+			<div>
+				<Navbar color="light" light expand="md">
+					<NavbarBrand href="/">React-Landing-Page</NavbarBrand>
+					<NavbarToggler onClick={this.toggle} />
+					<Collapse isOpen={this.state.isOpen} navbar>
+						<Nav className="ml-auto" navbar>
+							<NavItem>
+								<NavLink href="/components/">Components</NavLink>
+							</NavItem>
+							<NavItem>
+								<NavLink href="https://github.com/cxjay26/">GitHub</NavLink>
+							</NavItem>
+							<UncontrolledDropdown nav inNavbar>
+								<DropdownToggle nav caret>
+									Options
+								</DropdownToggle>
+								<DropdownMenu right>
+									<DropdownItem>Option 1</DropdownItem>
+									<DropdownItem>Option 2</DropdownItem>
+									<DropdownItem divider />
+									<DropdownItem>Reset</DropdownItem>
+								</DropdownMenu>
+							</UncontrolledDropdown>
+						</Nav>
+					</Collapse>
+				</Navbar>
+			</div>
 		);
 	}
 }
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,18 +1,15 @@
 import React from "react";
 import rigoImage from "../../img/rigo-baby.jpg";
 import "../../styles/home.scss";
+import { Hero } from "../component/hero";
+import { CardsGroup } from "../component/cardgroup";
 
 export class Home extends React.Component {
 	render() {
 		return (
-			<div className="text-center mt-5">
-				<h1>Hello Rigo!</h1>
-				<p>
-					<img src={rigoImage} />
-				</p>
-				<a href="#" className="btn btn-success">
-					If you see this green button, bootstrap is working
-				</a>
+			<div>
+				<Hero />
+				<CardsGroup />
 			</div>
 		);
 	}
